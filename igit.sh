753588a53d45fe75e23cdc1a14729e901e24df27
tiiaan <wong.haotian@foxmail.com>
2021-09-12 23:31:27 +0800

    update

--- igit.sh
+++ igit.sh
@@ -1,30 +1,28 @@
 #!/bin/bash
-# cd /home/tiiaan/Public/sandbox/alzheimer
-# git add .
-# git commit -m 'update'
-# git pull origin master
-# git push -u origin master
-# echo "----------------------------------------"
+cd /home/tiiaan/Public/sandbox/alzheimer
+git add .
+git commit -m 'update'
+git pull origin master
+git push -u origin master
+echo "----------------------------------------"
 
-# cd /home/tiiaan/Public/sandbox/soft-night-vision
-# git add .
-# git commit -m 'update'
-# git pull origin master
-# git push -u origin master
-# echo "----------------------------------------"
+cd /home/tiiaan/Public/sandbox/soft-night-vision
+git add .
+git commit -m 'update'
+git pull origin master
+git push -u origin master
+echo "----------------------------------------"
 
-# cd /home/tiiaan/Public/sandbox/ubuntu-scaffold
-# git add .
-# git commit -m 'update'
-# git pull origin master
-# git push -u origin master
-# echo "----------------------------------------"
+cd /home/tiiaan/Public/sandbox/ubuntu-scaffold
+git add .
+git commit -m 'update'
+git pull origin master
+git push -u origin master
+echo "----------------------------------------"
 
-for file in /home/tiiaan/Public/*
-do
-    git add .
-    git commit -m 'update'
-    git pull origin master
-    git push -u origin master
-    echo "${PWD##*/}"
-done
+cd /home/tiiaan/Public/sandbox/easy-git
+git add .
+git commit -m 'update'
+git pull origin master
+git push -u origin master
+echo "----------------------------------------"
--- ipull.sh
+++ ipull.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
-# cd /home/tiiaan/Public/sandbox/alzheimer
-# git pull origin master
-# echo "----------------------------------------"
-# cd /home/tiiaan/Public/sandbox/soft-night-vision
-# git pull origin master
-# echo "----------------------------------------"
-# cd /home/tiiaan/Public/sandbox/ubuntu-scaffold
-# git pull origin master
-# echo "----------------------------------------"
-
-for file in /home/tiiaan/Public/*
-    do
-    git pull origin master
-    done
+cd /home/tiiaan/Public/sandbox/alzheimer
+git pull origin master
+echo "----------------------------------------"
+cd /home/tiiaan/Public/sandbox/soft-night-vision
+git pull origin master
+echo "----------------------------------------"
+cd /home/tiiaan/Public/sandbox/ubuntu-scaffold
+git pull origin master
+echo "----------------------------------------"
+cd /home/tiiaan/Public/sandbox/easy-git
+git pull origin master
+echo "----------------------------------------"
