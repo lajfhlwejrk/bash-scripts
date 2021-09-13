0365f1702d227b15b9e739601a7e6163d002a91a
tiiaan <wong.haotian@foxmail.com>
2021-09-13 09:40:25 +0800

    update

--- igit.sh
+++ igit.sh
@@ -1,28 +1,39 @@
 #!/bin/bash
-cd /home/tiiaan/Public/sandbox/alzheimer
-git add .
-git commit -m 'update'
-git pull origin master
-git push -u origin master
-echo "----------------------------------------"
+# cd /home/tiiaan/Public/sandbox/alzheimer
+# git add .
+# git commit -m 'update'
+# git pull origin master
+# git push -u origin master
+# echo "----------------------------------------"
 
-cd /home/tiiaan/Public/sandbox/soft-night-vision
-git add .
-git commit -m 'update'
-git pull origin master
-git push -u origin master
-echo "----------------------------------------"
+# cd /home/tiiaan/Public/sandbox/soft-night-vision
+# git add .
+# git commit -m 'update'
+# git pull origin master
+# git push -u origin master
+# echo "----------------------------------------"
 
-cd /home/tiiaan/Public/sandbox/ubuntu-scaffold
-git add .
-git commit -m 'update'
-git pull origin master
-git push -u origin master
-echo "----------------------------------------"
+# cd /home/tiiaan/Public/sandbox/ubuntu-scaffold
+# git add .
+# git commit -m 'update'
+# git pull origin master
+# git push -u origin master
+# echo "----------------------------------------"
 
-cd /home/tiiaan/Public/sandbox/easy-git
-git add .
-git commit -m 'update'
-git pull origin master
-git push -u origin master
-echo "----------------------------------------"
+# cd /home/tiiaan/Public/sandbox/easy-git
+# git add .
+# git commit -m 'update'
+# git pull origin master
+# git push -u origin master
+# echo "----------------------------------------"
+x=$(find /home/tiiaan/Public -type d -name .git)
+for i in $x; do
+    cd $i 
+    cd ..
+    echo ${PWD##*/}
+    git add .
+    git commit -m 'update'
+    git pull origin master
+    git push -u origin master
+    echo "------------------------------------------------"
+done
--- ipull.sh
+++ ipull.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
-cd /home/tiiaan/Public/sandbox/alzheimer
-git pull origin master
-echo "----------------------------------------"
-cd /home/tiiaan/Public/sandbox/soft-night-vision
-git pull origin master
-echo "----------------------------------------"
-cd /home/tiiaan/Public/sandbox/ubuntu-scaffold
-git pull origin master
-echo "----------------------------------------"
-cd /home/tiiaan/Public/sandbox/easy-git
-git pull origin master
-echo "----------------------------------------"
+x=$(find /home/tiiaan/Public -type d -name .git)
+for i in $x; do
+    cd $i 
+    cd ..
+    echo ${PWD##*/}
+    git pull origin master
+    echo "------------------------------------------------"
+done
